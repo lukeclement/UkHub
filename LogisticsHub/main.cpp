55f1ed5c68f4818bc8e9ce9dd8c58c70eb4f83c8
lukeclement <masterclement@gmail.com>
2018-11-26 17:27:56 +0000

    Initial Commit

---
+++ LogisticsHub/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LogisticsHub
+//
+//  Created by Luke Clement on 26/11/2018.
+//  Copyright © 2018 Luke Clement. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
