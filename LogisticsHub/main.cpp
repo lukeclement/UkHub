5e312e97a4f9b1988b2d31f2f89894d85d2ce7bc
lukeclement <masterclement@gmail.com>
2018-12-03 10:01:13 +0000

    Did a bit of spring cleaning!

--- LogisticsHub/main.cpp
+++ LogisticsHub/main.cpp
@@ -26,9 +26,10 @@ struct hub{
     int servicing;
 };
 
-//Structure for [STUFF]
+//Structure for many hubs
 struct collection{
     double fitness;
+    //Hub connections
     vector<int> connections;
 };
 
@@ -42,13 +43,11 @@ struct bounds{
 
 //Structure containing data for the optimal hub
 struct opInfo{
-    int bestHub;
-    int bestFit;
     int iterations;
     vector<hub> finals;
     collection addon;
 };
-
+//Structure to contain data on places
 struct placesInfo{
     vector<vector<double> > nums;
     vector<vector<string> > names;
@@ -62,36 +61,38 @@ placesInfo getPlaces(string fileName){
     string line;
     string number;
     //Places have only numbers in [0, 0, pop, lat, long]
+    //Names has raw string data in [name, type, pop, lat, long]
     vector< vector <double> > places;
     vector< vector <string> > names;
-    vector<string> raw;
     vector<double> data;
+    vector<string> raw;
+    
     int start, end, x;
-    bool last=false;
+    bool last = false;
+    
     if(file.is_open()){
-        cout << "Reading file...\n";
+        cout << "Reading file " << fileName << "...\n";
         //Opening file
         x = 0;
         while(!file.eof()){
             //Getting line
             getline(file, line);
-            if(x!=0){
+            if(x != 0){
                 //Start and end points of commas
                 start = 0;
                 end = 0;
                 //Going through each line
-                last=false;
+                last = false;
                 for(int i = 0; end != -1; i++){
                     //Finding next comma
                     end = (int)line.find(",", i);
                     //Checking validity
                     if(end - start > 0){
-                        last=true;
+                        last = true;
                         //Getting number and adding to data
                         number = line.substr(start, end - start);
                         data.push_back(atof(number.c_str()));
                         raw.push_back(number);
-                        //cout << atof(number.c_str()) << ", ";
                         
                     }else if(end == -1&&last){
                         //Hit the end of the line
@@ -100,7 +101,6 @@ placesInfo getPlaces(string fileName){
                         number = line.substr(start, end-start);
                         data.push_back(atof(number.c_str()));
                         raw.push_back(number);
-                        //cout << atof(number.c_str());
                         //Setting end to -1 to end loop
                         end = -1;
                     }
@@ -113,22 +113,22 @@ placesInfo getPlaces(string fileName){
                     names.push_back(raw);
                     data.clear();
                     raw.clear();
-                    //cout << "\n";
                 }
             }
             x++;
         }
     }else{
         //Error message
-        cout << fileName << " file didn't open!" << "\n";
+        cout << fileName << " didn't open!" << "\n";
         exit(1);
     }
     
     //Closing file
     file.close();
-    cout << "Got all the places!\n";
-    output.nums=places;
-    output.names=names;
+    cout << "Found all " << places.size() << " entries!\n";
+    
+    output.nums = places;
+    output.names = names;
     return output;
 }
 
@@ -137,186 +137,188 @@ bounds getBounds(vector< vector<double> > places){
     bounds newBounds;
     
     //Finding boundaries for lat/long
-    double maxLat=-INFINITY;
-    double minLat=INFINITY;
-    double maxLong=-INFINITY;
-    double minLong=INFINITY;
-    for(int i=0;i<places.size();i++){
-        //cout << places[i][3] << "\n";
-        if(minLat>places[i][3]){
-            minLat=places[i][3];
-        }if(maxLat<places[i][3]){
-            maxLat=places[i][3];
+    double maxLat = -INFINITY;
+    double minLat = INFINITY;
+    double maxLong = -INFINITY;
+    double minLong = INFINITY;
+    //scanning through lat/long values
+    for(int i = 0; i < places.size(); i++){
+        if(minLat > places[i][3]){
+            minLat = places[i][3];
+        }if(maxLat < places[i][3]){
+            maxLat = places[i][3];
         }
-        if(minLong>places[i][4]){
-            minLong=places[i][4];
-        }if(maxLong<places[i][4]){
-            maxLong=places[i][4];
+        if(minLong > places[i][4]){
+            minLong = places[i][4];
+        }if(maxLong < places[i][4]){
+            maxLong = places[i][4];
         }
     }
-    newBounds.maxLat=maxLat;
-    newBounds.minLat=minLat;
-    newBounds.maxLong=maxLong;
-    newBounds.minLong=minLong;
+    //Setting variable values
+    newBounds.maxLat = maxLat;
+    newBounds.minLat = minLat;
+    newBounds.maxLong = maxLong;
+    newBounds.minLong = minLong;
     return newBounds;
 }
 
 //Finding Weighted distances
 double hDist(double startLat, double endLat, double startLong, double endLong, double weight){
-    double convert=M_PI/180.0;
-    double R=6371.01;
-    double ela=endLat*convert;
-    double sla=startLat*convert;
-    double elo=endLong*convert;
-    double slo=startLong*convert;
-    
-    double Dlat=(ela-sla);
-    double Dlong=(elo-slo);
+    //Setting conversion constant and radius of Earth
+    double convert = M_PI/180.0;
+    double R = 6371.01;
+    //Converting all the amounts
+    double ela = endLat*convert;
+    double sla = startLat*convert;
+    double elo = endLong*convert;
+    double slo = startLong*convert;
+    //Actually calculating haversine distance
+    double Dlat = (ela-sla);
+    double Dlong = (elo-slo);
     
     double a = pow(sin(Dlat/2) , 2) + cos(sla)*cos(ela)*pow(sin(Dlong/2) , 2);
     
     double b = 2*atan2(sqrt(a) , sqrt(1.0-a));
     
-    
+    //Multipling distance by weight factor
     return R*b*weight;
 }
 
 //Finding the fitness of a single hub
 double findFitness(double lat, double lon, vector< vector<double> > places){
-    double fitness=0;
-    for(int i=0;i<places.size();i++){
-        fitness+=hDist(lat, places[i][3], lon, places[i][4], places[i][2]);
+    //Inital fitness is zero
+    double fitness = 0;
+    for(int i=0; i < places.size(); i++){
+        //Add the weighted distance to each place
+        fitness += hDist(lat, places[i][3], lon, places[i][4], places[i][2]);
     }
     return fitness;
 }
 
 //Finding the fitness of multiple hubs, all connected to different places and those connections
-collection findDualFitnesses(vector<hub> dualHubs, vector< vector<double> > places){
-    double bestFitness;
-    double fitness=0;
-    vector<int> connections;
-    double best=0;
+collection findDualFitnesses(vector< hub > dualHubs, vector< vector<double> > places){
+    //setting variables
+    collection col;
     
-    for(int i=0;i<places.size();i++){
-        bestFitness=INFINITY;
-        for(int j=0;j<dualHubs.size();j++){
-            //cout << "Checking hub " << j << "(Fitness:"<<hDist(dualHubs[j].lat, places[i][3], dualHubs[j].lon, places[i][4], places[i][2])<<")\n";
-            if(bestFitness>hDist(dualHubs[j].lat, places[i][3], dualHubs[j].lon, places[i][4], places[i][2])){
-                //cout << "Accessed\n";
-                bestFitness=hDist(dualHubs[j].lat, places[i][3], dualHubs[j].lon, places[i][4], places[i][2]);
-                best=j;
+    vector< int > connections;
+    
+    double bestFitness;
+    double fitness = 0;
+    int best;
+    //Starting the loop
+    for(int i = 0; i < places.size(); i++){
+        //Best fitness so far is unknown
+        bestFitness = INFINITY;
+        //Loop through all the hubs
+        for(int j = 0; j < dualHubs.size(); j++){
+            //Looking for best fitness
+            if(bestFitness > hDist(dualHubs[j].lat, places[i][3], dualHubs[j].lon, places[i][4], places[i][2])){
+                //letting the program know a best fitness so far has been found
+                bestFitness = hDist(dualHubs[j].lat, places[i][3], dualHubs[j].lon, places[i][4], places[i][2]);
+                best = j;
             }
         }
-        //cout << "Current Fitness=" << fitness << "\n";
-        fitness+=bestFitness;
+        //Adding on the best connection to the pile
+        fitness += bestFitness;
         connections.push_back(best);
     }
-    collection col;
-    col.connections=connections;
-    col.fitness=fitness;
+    //Outputting results
+    col.connections = connections;
+    col.fitness = fitness;
     return col;
 }
 
 //Optimising for _____single______ hub
 opInfo optimise(vector< hub > hubs, vector< vector<double> > places){
     opInfo output;
-    vector<hub> newHubs=hubs;
+    vector< hub > newHubs = hubs;
     
     //Relocating hubs
-    int bestHub=0;
-    double bestFitness=INFINITY;
-    double search=0.001;
+    double search = 0.001;
     double currentFit, testFit, dx, dy;
-    bool changing=true;
-    int iterations=0;
+    bool changing = true;
+    int iterations = 0;
     
-    //Finding minimum of hubs
-    //cout << "Phase 1\n";
+    //Starting the loop to find local minimum(s)
     while(changing){
-        //cout << "Phase 2."<<iterations<<"\n";
+        //Seeing how many iterations it took
         iterations++;
-        changing=false;
-        //Getting best hubs
-        for(int i=0;i<newHubs.size();i++){
-            if(bestFitness>newHubs[i].fitness){
-                bestHub=i;
-                bestFitness=newHubs[i].fitness;
-            }
-        }
-        
-        for(int k=0;k<newHubs.size();k++){
-            dx=0;
-            dy=0;
-            
-            currentFit=newHubs[k].fitness;
-            for(int i=-10;i<=10;i++){
-                for(int j=-10;j<=10;j++){
-                    if(i!=0 || j!=0){
-                        testFit=findFitness(newHubs[k].lat+i*search, newHubs[k].lon+j*search, places);
-                        
-                        if(testFit<currentFit){
-                            changing=true;
-                            dx=i*search;
-                            
-                            dy=j*search;
-                            currentFit=testFit;
-                            
+        changing = false;
+        //Looping through the hubs
+        for(int k = 0; k < newHubs.size(); k++){
+            dx = 0;
+            dy = 0;
+            //Getting start fitness
+            currentFit = newHubs[k].fitness;
+            for(int i = -10; i <= 10; i++){
+                for(int j = -10; j <= 10; j++){
+                    if(i != 0 || j != 0){
+                        //Having a look around
+                        testFit = findFitness(newHubs[k].lat + i*search, newHubs[k].lon + j*search, places);
+                        //Seeing if found a better fit
+                        if(testFit < currentFit){
+                            changing = true;
+                            dx = i*search;
+                            dy = j*search;
+                            currentFit = testFit;
                         }
                     }
                 }
             }
-            newHubs[k].lat+=dx;
-            newHubs[k].lon+=dy;
+            //Editing hub
+            newHubs[k].lat += dx;
+            newHubs[k].lon += dy;
             newHubs[k].fitness=currentFit;
-            //cout << currentFit << "\n";
         }
-        
     }
-    output.iterations=iterations;
-    output.finals=newHubs;
-    output.bestFit=bestFitness;
-    output.bestHub=bestHub;
-    output.iterations=iterations;
+    //Output results
+    output.iterations = iterations;
+    output.finals = newHubs;
+    output.iterations = iterations;
     return output;
 }
 
 //Optimising for _____multiple______ hubs
-opInfo multiBALL(vector<hub> oldHubs, vector<vector<double> > places, int minMax){
+opInfo multiBALL(vector< hub > oldHubs, vector< vector<double> > places, int minMax){
+    //Setting variables
     opInfo output;
-    vector<hub> hubs = oldHubs;
-    vector<hub> testHubs;
     collection standing;
     collection newStanding;
-    int iterations=0;
+    
+    vector<hub> hubs = oldHubs;
+    vector<hub> testHubs;
     vector< vector<double> > subPlaces;
-    standing=findDualFitnesses(hubs, places);
-    newStanding=findDualFitnesses(hubs, places);
     
-    bool testing=true;
-    
-    while (testing) {
-        testing=false;
+    int iterations = 0;
+    bool testing = true;
+    //Setting standing fitness and also starting new standing fitness
+    standing = findDualFitnesses(hubs, places);
+    newStanding = findDualFitnesses(hubs, places);
+    //Staring loop
+    while(testing) {
+        testing = false;
         iterations++;
-        
-        for(int i=0;i<hubs.size();i++){
-            
-            for(int j=0;j<standing.connections.size();j++){
-                if(standing.connections[j]==i){
+        //Looping through each hub
+        for(int i = 0; i < hubs.size(); i++){
+            //Getting conenctions, making each hub optimised for the cities it is connected to
+            for(int j = 0; j < standing.connections.size(); j++){
+                if(standing.connections[j] == i){
                     subPlaces.push_back(places[j]);
                     
                 }
             }
+            //Optimising each hub
             testHubs.push_back(hubs[i]);
-            hubs[i]=optimise(testHubs, subPlaces).finals[0];
+            hubs[i] = optimise(testHubs, subPlaces).finals[0];
             testHubs.clear();
             subPlaces.clear();
         }
-        newStanding=findDualFitnesses(hubs, places);
-        
+        //Making a new standing
+        newStanding = findDualFitnesses(hubs, places);
+        //Checking to see if new fitness is better than the old one
         if(newStanding.fitness<standing.fitness){
-            testing=true;
-            standing=findDualFitnesses(hubs, places);
-            //cout << standing.fitness << "\r";
+            testing = true;
+            standing = findDualFitnesses(hubs, places);
         }
     }
     
@@ -338,10 +340,10 @@ vector<hub> getHubs(bounds boundaries, int numOfHubs, vector<vector<double> > pl
     hub alpha;
     //Setting random hubs
     for(int i = 0; i < numOfHubs; i++){
-        alpha.lat=distLat(generate);
-        alpha.lon=distLong(generate);
-        alpha.fitness=findFitness(alpha.lat, alpha.lon, places);
-        alpha.servicing=0;
+        alpha.lat = distLat(generate);
+        alpha.lon = distLong(generate);
+        alpha.fitness = findFitness(alpha.lat, alpha.lon, places);
+        alpha.servicing = 0;
         //for(int j=0;j<places.size();j++){
         //    alpha.servicing+=places[j][2];
         //}
@@ -352,66 +354,53 @@ vector<hub> getHubs(bounds boundaries, int numOfHubs, vector<vector<double> > pl
 
 //Starting up program
 int main(int argc, const char * argv[]) {
-    
-    string fileName="GBplaces.csv";
-    //string fileName="Test.csv";
-    //string fileName="USplaces.csv";
+    opInfo moreData;
+    opInfo bestData;
     //Places have only numbers in [0, 0, pop, lat, long]
     vector< vector <double> > places;
-    //Raw text of csv
     vector< vector <string> > placeName;
-    placesInfo information=getPlaces(fileName);
-    places=information.nums;
-    placeName=information.names;
+    vector<hub> hubsA;
+    vector<hub> best;
+    
+    string fileName = "GBplaces.csv";
+    int nums, loops, q;
+    double servicing;
+    double bestFit = INFINITY;
+    //Reading file
+    placesInfo information = getPlaces(fileName);
+    places = information.nums;
+    placeName = information.names;
     
     vector<hub> hubs;
     
     //Find boundaries
-    bounds boundaries=getBounds(places);
+    bounds boundaries = getBounds(places);
     
     cout << "Min lat: " << boundaries.minLat << "; Max lat: " << boundaries.maxLat << "; Min long: " << boundaries.minLong << "; Max long: " << boundaries.maxLong << "\n";
-    
-    //Generating random hubs (nums)
-    int nums = 5;
-    hubs=getHubs(boundaries,5,places);
-    
-    
-    //Optimising
-    /*cout << "Finding best single hub...\n";
-    opInfo data=optimise(hubs, places);
-    hubs=data.finals;
-    //Saying what the best single hub is=
-    cout << "Best single hub found at: " << hubs[data.bestHub].lat << " , " << hubs[data.bestHub].lon << " , with a total node length of " << hubs[data.bestHub].fitness << ", after "<< data.iterations << " iterations\n";
-    */
+    //Requesting user input
     cout << "How many hubs would you like to place?\n";
     cout << ">>";
     cin >> nums;
     
-    vector<hub> hubsA;
-    vector<hub> best;
-    double bestFit=INFINITY;
-    opInfo moreData;
-    opInfo bestData;
-    int loops=20;
     cout << "How many loops would you like to test? (Higher is longer but can find better fits)\n";
     cout << ">>";
     cin >> loops;
-    for(int i=0;i<loops;i++){
+    
+    //Looking at each possibility
+    for(int i = 0; i < loops; i++){
         cout << "Observing possibility " << i << ", which has fitness ";
-        hubsA=getHubs(boundaries, nums, places);
-        moreData=multiBALL(hubsA, places, 10);
-        hubsA=moreData.finals;
+        hubsA = getHubs(boundaries, nums, places);
+        moreData = multiBALL(hubsA, places, 10);
+        hubsA = moreData.finals;
         cout << moreData.addon.fitness<< ", compared to the best fitness before this, "<< bestFit << "\n";
         if(moreData.addon.fitness<bestFit){
-            bestFit=moreData.addon.fitness;
-            best=hubsA;
-            bestData=moreData;
+            bestFit = moreData.addon.fitness;
+            best = hubsA;
+            bestData = moreData;
         }
     }
-    
     cout << "Complete!\n\n";
-    double servicing;
-    int q=0;
+    
     cout << "Found " << nums << " hubs, with a total node length of "<< bestData.addon.fitness <<"would you like to see:\n";
     cout << "[0]:   Hub locations\n";
     cout << "[1]:   Hub locations and connections\n";
@@ -420,28 +409,28 @@ int main(int argc, const char * argv[]) {
     cout << "[Else]:No output\n";
     cout << ">>";
     cin >> q;
-    for(int i=0;i<nums;i++){
-        if(q>=0&&q<=3){
+    
+    for(int i = 0; i < nums; i++){
+        if(q >= 0 && q <= 3){
             cout << "Hub found at: " << best[i].lat << " , " << best[i].lon << "\n";
         }
-        servicing=0;
-        if(q==1||q==2||q==3){
-            if(q!=3){
+        servicing = 0;
+        if(q == 1 || q == 2 || q == 3){
+            if(q != 3){
                 cout << "This hub is connected to:\n";
             }
-            for(int j=0;j<bestData.addon.connections.size();j++){
-                if(bestData.addon.connections[j]==i){
-                    if(q!=3){
+            for(int j = 0; j < bestData.addon.connections.size(); j++){
+                if(bestData.addon.connections[j] == i){
+                    if(q != 3){
                         cout << placeName[j][0] << "\n";
                     }
-                    servicing+=places[j][2];
+                    servicing += places[j][2];
                 }
             }
         }
-        if(q==2||q==3){
+        if(q == 2 || q == 3){
             cout << "Servicing " << servicing << " people\n";
         }
-        
     }
     
     return 0;
